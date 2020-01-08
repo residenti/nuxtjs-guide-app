7f9d5d565d4097f0da9d5c8c8fb057573c1cb8b0
residenti <n.queue.r@gmail.com>
2020-01-08 19:31:03 +0900

    はじめる > ルーティング > レイアウト > カスタムレイアウト.

---
+++ pages/posts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>/posts</h2>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blog' // カスタムレイアウト を指定.
+}
+</script>
