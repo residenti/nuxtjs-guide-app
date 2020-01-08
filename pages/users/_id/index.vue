fb5abec6a8200e1fa6629dcb70fc1c4d825190f2
residenti <n.queue.r@gmail.com>
2020-01-08 18:13:41 +0900

    はじめる > ルーティング > ルーティングのパラメータのバリデーション.

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -4,3 +4,12 @@
     <nuxt-link to="/">Home page</nuxt-link>
   </div>
 </template>
+
+<script>
+export default {
+  // もしバリデーションのメソッドが true または true に解決する Promise を返さない、またはエラーをスローした場合は、Nuxt.js は自動的に 404 エラーページあるいはエラーの場合 500 エラーページをロードします。
+  validate({ params }) {
+    return /^\d+$/.test(params.id) // 数値でなければならない
+  }
+}
+</script>
